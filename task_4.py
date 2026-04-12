33c425a1fbf78f406cfcecdc046227c0c20803fd
AlyonaS-QA <astadnikova074@gmail.com>
2026-04-12 22:08:31 +0300

    Спринт_2

---
+++ task_4.py
@@ -0,0 +1,31 @@
+class EmployeeSalary:
+
+    def __init__(self, name, hours, rest_days, email):
+        self.name = name
+        self.hours = EmployeeSalary.get_hours(hours,rest_days)
+        self.rest_days = rest_days
+        self.email = EmployeeSalary.get_email(name,email)
+        self.hourly_payment = 400
+
+    @staticmethod
+    def get_hours(hours,rest_days):
+        if not hours:
+            hours = (7 - rest_days) * 8
+            return hours
+        else:
+            return hours
+
+    @staticmethod
+    def get_email(name,email):
+        if not email:
+            email = f"{name}@email.com"
+            return email
+        else:
+            return email
+
+    def set_hourly_payment(self, hourly_payment):
+        self.hourly_payment = hourly_payment
+
+    def salary(self):
+        salary = self.hours * self.hourly_payment
+        return salary
