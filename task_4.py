a64c7cc0eb59986297aa4193078387da95eab686
AlyonaS-QA <astadnikova074@gmail.com>
2026-04-13 23:22:14 +0300

    fix-2 task_4

--- task_4.py
+++ task_4.py
@@ -1,26 +1,26 @@
 class EmployeeSalary:
+    hourly_payment = 400
 
     def __init__(self, name, hours, rest_days, email):
         self.name = name
         self.hours = EmployeeSalary.get_hours(hours,rest_days)
         self.rest_days = rest_days
         self.email = EmployeeSalary.get_email(name,email)
-        self.hourly_payment = 400
 
-    @classmethod
-    def get_hours(cls,hours,rest_days):
+    def get_hours(hours,rest_days):
         if not hours:
             hours = (7 - rest_days) * 8
         return hours
 
-    @classmethod
-    def get_email(cls,name,email):
+    def get_email(name,email):
         if not email:
             email = f"{name}@email.com"
         return email
 
-    def set_hourly_payment(self, hourly_payment):
-        self.hourly_payment = hourly_payment
+    @classmethod
+    def set_hourly_payment(cls, new_payment):
+        cls.hourly_payment = new_payment
+
 
     def salary(self):
         salary = self.hours * self.hourly_payment
